828c4977154d9f8bdb43a317fc47ed6e221f2eac
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2023-01-15 18:17:23 +0100

    Prod ready

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from src.app import app
+
+app_mode = os.getenv('MODE')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if(app_mode == "dev"):
+        app.run(debug=True)
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8080)
